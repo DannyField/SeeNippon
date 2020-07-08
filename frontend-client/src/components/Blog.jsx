9ee6a0d27d967f9ebdeed6ef1085f84cb1699ddc
DannyField <dannyfieldproductions@gmail.com>
2020-07-08 16:37:05 +1000

    added blog single post

--- frontend-client/src/components/Blog.jsx
+++ frontend-client/src/components/Blog.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Blog() {
-  return <div></div>;
+class Blog extends React.Component {
+  deleteBlog = async (id) => {
+    await fetch(`http://localhost:3000/blogs/${id}`, {
+      method: "DELETE",
+    });
+    window.alert("Deleted The Post! Why!!");
+    this.props.history.push("/blogs");
+  };
+
+  render() {
+    const b = this.props.location.state;
+    console.log(this.props);
+    return (
+      <div>
+        <h1>{b.title}</h1>
+        <button onClick={this.props.history.goBack}>Back</button>
+        <Link to={`/blogs/${b.id}/edit`}>
+          <button>Edit</button>
+        </Link>
+        <span onClick={() => this.deleteThing(b.id)}>
+          <button>Delete</button>
+        </span>
+      </div>
+    );
+  }
 }
+
+export default Blog;
