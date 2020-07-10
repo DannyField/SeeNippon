36e6ce91b9b4de15551a4bc3688dde8f5caa5f04
DannyField <dannyfieldproductions@gmail.com>
2020-07-10 10:04:05 +1000

    weather added to cards displayed

--- frontend-client/src/components/Card.jsx
+++ frontend-client/src/components/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../stylesheets/Card.css";
 
 class Card extends React.Component {
-
   async componentDidMount() {
     const response = await fetch(
       "http://api.weatherstack.com/current?access_key=38dc0a08ba364082a8994237ba2f5d65&query=Tokyo"
@@ -12,51 +11,55 @@ class Card extends React.Component {
     let precip = JSON.stringify(data.current.precip);
     let humidity = JSON.stringify(data.current.humidity);
     let uvIndex = JSON.stringify(data.current.uv_index);
-    this.setState({ weather: data, currentTemp: currentTemp, precip: precip, humidity: humidity, uvIndex: uvIndex });
+    this.setState({
+      weather: data,
+      currentTemp: currentTemp,
+      precip: precip,
+      humidity: humidity,
+      uvIndex: uvIndex,
+    });
     // console.log(this.state);
-  };
+  }
 
   render() {
     const weather = this.state?.weather;
     if (weather === undefined) {
-      return <p>Weather is loading please wait..</p>
+      return <p>Weather is loading please wait..</p>;
     } else {
       console.log(weather);
       return (
-        <div>
-          <h3>Current Weather in Tokyo:</h3>
-          <p>Current Temperature: {this.state.currentTemp}</p>
-          <p>Precipitation: {this.state.precip}</p>
-          <p>Humidity: {this.state.humidity}</p>
-          <p>UV Index: {this.state.uvIndex}</p>
+        <div className="card">
+          <div className="JapanInfo">
+            <h1>Come to the land of the rising sun!</h1>
+            <p>
+              Enjoy sake with the locals. Visit the shrines hidden away in lush
+              landscapes
+            </p>
+            <br></br>
+            <p>
+              Japan is an island country in East Asia located in the northwest
+              Pacific Ocean. It borders the Sea of Japan to the west, and it
+              extends from the Sea of Okhotsk in the north to the East China Sea
+              and Taiwan in the south
+            </p>
+            <br></br>
+            <p>
+              Japan Airlines is the national carrier and offers daily cheap
+              flights to Tokyo and Osaka from Sydney. Other airlines that
+              operate flights to Japan are: China Southern, Air Asia, Qantas,
+              Thai Airways and Jetstar.
+            </p>
+          </div>
+          <div className="weather-Container">
+            <h3>Current Weather in Tokyo:</h3>
+            <p>Current Temperature: {this.state.currentTemp}</p>
+            <p>Precipitation: {this.state.precip}</p>
+            <p>Humidity: {this.state.humidity}</p>
+            <p>UV Index: {this.state.uvIndex}</p>
+          </div>
         </div>
       );
     }
-    return (
-      <div className="card">
-        <div className="JapanInfo">
-          <h1>Come to the land of the rising sun!</h1>
-          <p>
-            Enjoy sake with the locals. Visit the shrines hidden away in lush
-            landscapes
-          </p>
-          <br></br>
-          <p>
-            Japan is an island country in East Asia located in the northwest
-            Pacific Ocean. It borders the Sea of Japan to the west, and it
-            extends from the Sea of Okhotsk in the north to the East China Sea
-            and Taiwan in the south
-          </p>
-          <br></br>
-          <p>
-            Japan Airlines is the national carrier and offers daily cheap
-            flights to Tokyo and Osaka from Sydney. Other airlines that operate
-            flights to Japan are: China Southern, Air Asia, Qantas, Thai Airways
-            and Jetstar.
-          </p>
-        </div>
-      </div>
-    );
   }
 }
 export default Card;
