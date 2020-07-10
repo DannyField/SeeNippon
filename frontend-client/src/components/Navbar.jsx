53b83c36f9995f04a4b1b1082e73e41856dd49a6
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-10 10:32:24 +1000

    conditional rendering login logout and sign up

--- frontend-client/src/components/Navbar.jsx
+++ frontend-client/src/components/Navbar.jsx
@@ -5,10 +5,35 @@ import '../stylesheets/japan.css'
 
 const Navbar = () => {
   const history = useHistory();
+  console.log(localStorage.getItem("token"))
 
   // render() {
+    if(localStorage.getItem("token")!==null){
     return (
-
+      <nav className="flex-nav">
+        <div className="jp-container">
+          <div className="grid">
+            <div className="column-xs-9 column-md-6">
+              <p id="logo">See Nippon!<span id="highlight">.</span></p>
+            </div>
+            <div className="column-xs-3 column-md-6">
+              <a href="#" className="toggle-nav">Menu <i className="ion-navicon-round"></i></a>
+              <ul>
+                <li><Link className="link" to="/">Home</Link></li>
+                <li><Link className="link" to="/blogs">All Blogs</Link></li>
+                <li><Link className="link" to="/blogs/create">Create</Link></li>
+                {/* <li><Link className="link" to="/login">Login</Link></li>
+                <li><Link className="link" to="/sign-up">Sign Up</Link></li> */}
+                <li><span onClick={() => {localStorage.removeItem("token");
+                history.push("/login");}}>Logout</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+    }else{
+      return (
       <nav className="flex-nav">
         <div className="jp-container">
           <div className="grid">
@@ -23,14 +48,15 @@ const Navbar = () => {
                 <li><Link className="link" to="/blogs/create">Create</Link></li>
                 <li><Link className="link" to="/login">Login</Link></li>
                 <li><Link className="link" to="/sign-up">Sign Up</Link></li>
-                <li><span onClick={() => {localStorage.removeItem("token");
-                history.push("/login");}}>Logout</span></li>
+                {/* <li><span onClick={() => {localStorage.removeItem("token");
+                history.push("/login");}}>Logout</span></li> */}
               </ul>
             </div>
           </div>
         </div>
-      </nav>
-    );
+      </nav>  
+      )
+    }
   }
 
 export default Navbar;
